df9e73a2c207aff222ef6381298114f8c920dd8f
CesarGalarza <20190779@aloe.ulima.edu.pe>
2022-03-03 15:26:53 -0500

    Final Final 2

---
+++ sequelize/seeders/20220303202325-admin.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /*
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert("Usuario", [
+      {
+        email: "marcelo@admin.com",
+        password: "123",
+        createdAt : new Date(),
+        updatedAt : new Date()
+      },
+
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /*
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('Client', null, {});
+     */
+     await queryInterface.bulkDelete('User', null, {});
+  }
+};
