991b8c83056cf5e52e560c7487ac0df16be83c18
CesarGalarza <20190779@aloe.ulima.edu.pe>
2022-03-03 15:34:18 -0500

    Final Final 2

--- pages/editar_tipo_de_cambio.js
+++ pages/editar_tipo_de_cambio.js
@@ -1,47 +1,15 @@
-import { useState } from "react"
-import Comprar_Vender from "./comprar_vender"
+import { useEffect, useState } from "react"
+import { guardarTC, obtenerTC } from "../dao/proyectos_local"
+import Tipo_Cambio from "../components/Tipo_Cambio.component"
 
 export default function editar_tipo_de_cambio(props) {
-    const [txtPEN, setTxtPEN] = useState("")
-    const [txtBTC, setTxtBTC] = useState("")
 
-    const txtPENOnChange = (event) => {
-        const txtPENIngresado = event.target.value
-        setTxtCorreo(txtPENIngresado)
-        //setTxtUsername("")
-    }
-
-    const txtBTCOnChange = (event) => {
-        setTxtBTC(event.target.value)//lo mismo de arriba
-    }
-
-    const butCambiarOnClick = () => {
-        Comprar_Vender.MostrarTipoDeCambio(txtBTC)
-    }
-    return <div className="card col-lg-5 col-md-6 col-sm-8 col-9">
-        <div className="card-body">
-            <div>
-                <div className="card mt-2" style={{ width: 18 + 'rem' }}>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">
-                            <input type="text" defaultValue={txtPEN}
-                                onChange={txtPENOnChange} />
-                            &nbsp;
-                            &nbsp;
-                            <span>PEN</span>
-                        </li>
-                        <li className="list-group-item">
-                            <input type="text" defaultValue={txtBTC}
-                                onChange={txtBTCOnChange} />
-                            &nbsp;
-                            &nbsp;
-                            <span>BTC</span>
-                        </li>
-                    </ul>
+    return <div>
+        <div className="card col-md-3 mt-4">
+            <div className="card-body">
+                <div>
+                    <Tipo_Cambio  mostrar={true}/>
                 </div>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <button type="button" className="mt-2" onClick={butCambiarOnClick}>Cambiar</button>
             </div>
         </div>
     </div>
